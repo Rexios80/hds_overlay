44c4f0591bcb754f59a69947607c4c45869fb35b
Aaron DeLory <rexiosdev@gmail.com>
2019-10-14 12:42:18 -0400

    SocketServer works

--- public/app.js
+++ public/app.js
@@ -1,3 +1,4 @@
+let WebSocket = require('ws');
 let express = require('express');
 let app = express();
 
@@ -11,4 +12,16 @@ let server = app.listen(8080, function () {
     let port = server.address().port;
 
     console.log('Listening at port %s', port);
+});
+
+// This wouldn't work in it's own file for some reason
+let wss = new WebSocket.Server({ port: 3476 });
+
+wss.on('connection', function connection(ws) {
+    console.log('Client connected');
+    ws.on('message', function incoming(message) {
+        console.log('received: %s', message);
+    });
+
+    ws.send('something');
 });
