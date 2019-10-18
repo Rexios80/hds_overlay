8b6d386c05d1c85550678068e958213778353e0f
Aaron DeLory <rexiosdev@gmail.com>
2019-10-18 01:18:00 -0400

    Reconnect client to server on disconnect

--- public/socketClient.js
+++ public/socketClient.js
@@ -1,26 +1,37 @@
-let socket = new WebSocket('ws://localhost:3476');
+function connect() {
+    let socket = new WebSocket('ws://localhost:3476');
 
-let heartRateText = null;
-let caloriesText = null;
-window.onload = function () {
-    heartRateText = document.querySelector('#heartRate');
-    caloriesText = document.querySelector('#calories');
-};
+    let heartRateText = null;
+    let caloriesText = null;
+    window.onload = function () {
+        heartRateText = document.querySelector('#heartRate');
+        caloriesText = document.querySelector('#calories');
+    };
 
-socket.onopen = function (event) {
-    console.log('Connected to server');
-};
+    socket.onopen = function (event) {
+        console.log('Connected to server');
+    };
 
-socket.onmessage = function (event) {
-    console.log(event.data);
+    socket.onclose = function (event) {
+        console.log('Disconnected from server');
+        setTimeout(function() {
+            connect();
+        }, 1000);
+    };
 
-    let data = event.data.split(':');
+    socket.onmessage = function (event) {
+        console.log(event.data);
 
-    if (data[0] === 'heartRate') {
-        heartRateText.textContent = 'Heart rate: ' + data[1];
-    }
+        let data = event.data.split(':');
 
-    if (data[0] === 'calories') {
-        caloriesText.textContent = 'Calories: ' + data[1];
-    }
-};
+        if (data[0] === 'heartRate') {
+            heartRateText.textContent = 'Heart rate: ' + data[1];
+        }
+
+        if (data[0] === 'calories') {
+            caloriesText.textContent = 'Calories: ' + data[1];
+        }
+    };
+}
+
+connect();
