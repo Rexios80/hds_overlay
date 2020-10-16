895e7b8acfe385bae5ed9b42d3c661729af7334a
Aaron DeLory <rexiosdev@gmail.com>
2020-10-16 03:42:52 -0400

    macOS version is now packaged with Platypus

--- buildscript.sh
+++ buildscript.sh
@@ -1,1 +1,4 @@
 pkg package.json --out-path output
+platypus macos-platypus-script output/HDS-Overlay-macos.app -y -R -f output/HDS-Overlay-macos -i macos-icon.icns -a "HDS-Overlay-macos"
+rm output/HDS-Overlay-macos
+./macossignscript.sh
