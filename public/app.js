3c662ae2f70e6edc67a275feda4b00a5e0071e2f
Aaron DeLory <rexiosdev@gmail.com>
2020-11-20 14:46:09 -0500

    Incremented version numbers

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-const version = '7.6.2'; // Make sure this matches the version in package.json
+const version = '8.0.0'; // Make sure this matches the version in package.json
 
 const WebSocket = require('ws');
 const express = require('express');
