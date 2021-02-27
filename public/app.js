5cd3f9fabdec90013b8259848fc9f4e388a7c3c8
Aaron DeLory <rexiosdev@gmail.com>
2021-02-27 15:03:49 -0500

    Updated version numbers

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-const version = '9.0.0'; // Make sure this matches the version in package.json
+const version = '9.1.0'; // Make sure this matches the version in package.json
 
 const WebSocket = require('ws');
 const express = require('express');
