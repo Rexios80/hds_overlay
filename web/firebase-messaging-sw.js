25531a7ee2ef88647837e2b416886c773f7b547b
Aaron DeLory <rexiosdev@gmail.com>
2021-05-11 12:13:33 -0400

    More secure firebase usage for web

--- web/firebase-messaging-sw.js
+++ web/firebase-messaging-sw.js
@@ -1,15 +1,5 @@
-importScripts("https://www.gstatic.com/firebasejs/8.5.0/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/8.5.0/firebase-messaging.js");
-
-firebase.initializeApp({
-  apiKey: "AIzaSyArjmNbUe6cnx9vJ5W65DIduUVoXMQpzNA",
-  authDomain: "hds.dev",
-  projectId: "health-data-server",
-  storageBucket: "health-data-server.appspot.com",
-  messagingSenderId: "47929674141",
-  appId: "1:47929674141:web:eb5d3073bfeb038f860774",
-  measurementId: "G-2YR45DMN6B",
-});
+importScripts("/__/firebase/8.5.0/firebase-app.js");
+importScripts("/__/firebase/8.5.0/firebase-messaging.js");
 
 const messaging = firebase.messaging();
 messaging.setBackgroundMessageHandler(function (payload) {
