1f8aea239e6da0b09f3dcbc7709b5ef2a1fb78b3
Aaron DeLory <rexiosdev@gmail.com>
2021-05-12 15:42:51 -0400

    Fixed RtdClient not allowing desktop builds

--- lib/controllers/connection_controller.dart
+++ lib/controllers/connection_controller.dart
@@ -6,7 +6,8 @@ import 'package:hds_overlay/hive/data_type.dart';
 import 'package:hds_overlay/model/log_message.dart';
 import 'package:hds_overlay/model/message.dart';
 import 'package:hds_overlay/services/connection/connection_base.dart';
-import 'package:hds_overlay/services/connection/rtd_client.dart';
+import 'package:hds_overlay/services/connection/rtd_client_stub.dart'
+    if (dart.library.js) 'package:hds_overlay/services/connection/rtd_client.dart';
 import 'package:hds_overlay/services/connection/socket_client_stub.dart'
     if (dart.library.js) 'package:hds_overlay/services/connection/socket_client.dart';
 import 'package:hds_overlay/services/connection/socket_server_stub.dart'
---
+++ lib/services/connection/rtd_client_stub.dart
@@ -0,0 +1,14 @@
+import 'connection_base.dart';
+
+class RtdClient extends ConnectionBase {
+  @override
+  Future<void> start(
+    int port,
+    String serverIp,
+    String clientName,
+    List<String> serverIps,
+    String overlayId,
+  ) {
+    throw UnimplementedError();
+  }
+}
