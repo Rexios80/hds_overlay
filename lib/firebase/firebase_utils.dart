21969ecab57a763e29a77237a01c3fb61ea85428
Aaron DeLory <rexiosdev@gmail.com>
2021-05-14 08:59:50 -0400

    Revert "Enabled Firebase App Check"
    This reverts commit fe3cd07389275d9d3139b88bf35090b6c4802ac5.
    
    Son of a bitch App Check doesn't support watchOS

--- lib/firebase/firebase_utils.dart
+++ lib/firebase/firebase_utils.dart
@@ -1,6 +1,3 @@
-// ignore: avoid_web_libraries_in_flutter
-import 'dart:js' as js;
-
 import 'package:firebase/firebase.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
@@ -28,9 +25,6 @@ class FirebaseUtils {
         appId: "1:47929674141:web:0606fd3354256f51860774",
         measurementId: "G-1V10QYSSHG",
       );
-
-      print('firebaseAppCheck');
-      js.context.callMethod('firebaseAppCheck');
     }
 
     final auth = FirebaseAuth.instance;
