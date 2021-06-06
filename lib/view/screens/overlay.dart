676aa99c1d17bff8f0060dcaaeac315783a5e1eb
Rexios <rexios80@gmail.com>
2021-06-06 16:56:08 -0400

    Ran the formatter

--- lib/view/screens/overlay.dart
+++ lib/view/screens/overlay.dart
@@ -11,7 +11,6 @@ import 'package:hds_overlay/controllers/settings_controller.dart';
 import 'package:hds_overlay/hive/data_type.dart';
 import 'package:hds_overlay/hive/overlay_profile.dart';
 import 'package:hds_overlay/model/data_source.dart';
-import 'package:hds_overlay/utils/themes.dart';
 import 'package:lifecycle/lifecycle.dart';
 import 'package:tuple/tuple.dart';
 
