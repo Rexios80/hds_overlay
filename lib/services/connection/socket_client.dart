444d5b04724462afbf930c7e24178012d4d2fb5d
Rexios <rexios80@gmail.com>
2021-06-06 16:01:32 -0400

    Registering overlays works again

--- lib/services/connection/socket_client.dart
+++ lib/services/connection/socket_client.dart
@@ -1,10 +1,14 @@
 import 'dart:async';
 import 'dart:convert';
 
+import 'package:firebase/firebase.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
+import 'package:hds_overlay/controllers/firebase_controller.dart';
 import 'package:hds_overlay/firebase/firebase_utils_stub.dart'
     if (dart.library.js) 'package:hds_overlay/firebase/firebase_utils.dart';
+import 'package:hds_overlay/firebase/rtd_constants.dart';
 import 'package:hds_overlay/model/log_message.dart';
 import 'package:hds_overlay/services/connection/connection_base.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
@@ -37,7 +41,7 @@ abstract class SocketClient extends ConnectionBase {
       if (this is LocalSocketClient) {
         log(LogLevel.error, 'Unable to connect to server: $ip');
       } else if (this is CloudSocketClient) {
-        log(LogLevel.hdsCloud, 'Unable to connect to HDS Cloud');
+        log(LogLevel.error, 'Unable to connect to HDS Cloud');
       }
       Future.delayed(Duration(seconds: 10), () => _connect());
       return Future.error('Unable to connect to server: $ip');
@@ -120,6 +124,9 @@ class LocalSocketClient extends SocketClient {
 
 class CloudSocketClient extends SocketClient {
   final FirebaseUtils firebase = Get.find();
+  late final DatabaseReference _ref;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseController _firebaseController = Get.find();
 
   @override
   Future<Uri> createUri() async {
@@ -127,6 +134,49 @@ class CloudSocketClient extends SocketClient {
     return Uri.parse(
       'wss://xcdj6tkeza.execute-api.us-east-1.amazonaws.com/dev?auth=$token&overlayId=$overlayId',
     );
+  }
+
+  CloudSocketClient() {
+    Database db = database();
+    _ref = db.ref(RtdConstants.overlays);
+  }
+
+  @override
+  Future<void> start(
+    String ip,
+    int port,
+    String clientName,
+    List<String> serverIps,
+    String overlayId,
+  ) async {
+    print('Requesting uidSnapshot');
+    final uidSnapshot =
+        (await _ref.child(overlayId).child(RtdConstants.uid).once('value'))
+            .snapshot;
+    print('uidSnapshot received');
+    print('HDS Cloud uid: ${uidSnapshot.val()}');
+    if (uidSnapshot.exists() && uidSnapshot.val() != _auth.currentUser?.uid) {
+      log(LogLevel.error, 'HDS Cloud ID collision detected');
+      log(LogLevel.error, 'Regenerating HDS Cloud ID...');
+      _firebaseController.regenerateOverlayId();
+      return start(
+        ip,
+        port,
+        clientName,
+        serverIps,
+        _firebaseController.config.value.overlayId,
+      );
+    } else if (!uidSnapshot.exists()) {
+      log(LogLevel.hdsCloud, 'Registering with HDS Cloud...');
+      await uidSnapshot.ref.set(_auth.currentUser?.uid).then((_) {
+        log(LogLevel.hdsCloud, 'Registered with HDS Cloud');
+      }).onError((error, stackTrace) {
+        print(error);
+        print(stackTrace);
+        log(LogLevel.error, 'Unable to register with HDS Cloud');
+      });
+    }
+    return super.start(ip, port, clientName, serverIps, overlayId);
   }
 
   @override
