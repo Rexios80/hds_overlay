2ae045f6bccefc3372fd33ea076283a4168bda69
Rexios <rexios80@gmail.com>
2021-06-08 17:34:42 -0400

    Updated app bar title for terms

--- lib/view/screens/terms.dart
+++ lib/view/screens/terms.dart
@@ -6,7 +6,7 @@ class Terms extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Privacy Policy'),
+        title: Text('Terms of Use'),
       ),
       body: Center(
         child: Card(
