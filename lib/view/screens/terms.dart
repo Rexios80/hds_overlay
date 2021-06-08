943e42e8ed31852d5836ed8a40aba7e7789dd27b
Rexios <rexios80@gmail.com>
2021-06-08 17:34:22 -0400

    Put the terms in a card

--- lib/view/screens/terms.dart
+++ lib/view/screens/terms.dart
@@ -8,14 +8,21 @@ class Terms extends StatelessWidget {
       appBar: AppBar(
         title: Text('Privacy Policy'),
       ),
-      body: ListView(
-        padding: EdgeInsets.all(10),
-        children: [
-          FutureBuilder(
-            future: rootBundle.loadString('assets/strings/terms'),
-            builder: (context, snap) => Text(snap.data as String),
+      body: Center(
+        child: Card(
+          child: Container(
+            constraints: BoxConstraints(maxWidth: 500),
+            child: ListView(
+              padding: EdgeInsets.all(10),
+              children: [
+                FutureBuilder(
+                  future: rootBundle.loadString('assets/strings/terms'),
+                  builder: (context, snap) => Text(snap.data as String),
+                ),
+              ],
+            ),
           ),
-        ],
+        ),
       ),
     );
   }
