7cbf94341a5574a2360d2396fabe0e8b859c252b
Rexios <rexios80@gmail.com>
2021-06-10 13:41:59 -0400

    Revert "Trying to fix firebase weirdness"
    This reverts commit f8390d6c6d3f4a480c5cf85034d509f64ac02fef.

--- lib/firebase/firebase_utils.dart
+++ lib/firebase/firebase_utils.dart
@@ -29,9 +29,6 @@ class FirebaseUtils {
         appId: "1:47929674141:web:0606fd3354256f51860774",
         measurementId: "G-1V10QYSSHG",
       );
-
-      print('firebaseAppCheck');
-      js.context.callMethod('firebaseAppCheck');
     }
 
     auth = FirebaseAuth.instance;
