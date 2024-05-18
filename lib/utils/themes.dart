75c38af4fa45f26088586f713d59e8c3f8af6498
Rexios <rexios@rexios.dev>
2024-05-18 09:50:27 -0400

    Update theme

--- lib/utils/themes.dart
+++ lib/utils/themes.dart
@@ -2,15 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:hds_overlay/utils/colors.dart';
 
 class Themes {
-  static final light = ThemeData(
-    brightness: Brightness.light,
-    primarySwatch: createMaterialColor(AppColors.accent),
-  );
+  static final light = ThemeData.light()
+      .copyWith(colorScheme: ColorScheme.fromSeed(seedColor: AppColors.accent));
 
-  static final dark = ThemeData(
-    brightness: Brightness.dark,
-    primarySwatch: createMaterialColor(AppColors.accent),
-  );
+  static final dark = ThemeData.dark()
+      .copyWith(colorScheme: ColorScheme.fromSeed(seedColor: AppColors.accent));
 
   static const sideBarWidth = 304.0;
 }
